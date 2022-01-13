ce7a93141b5c26fc9e586b4500571aa788a4b2dd
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2022-01-13 10:41:35 -0500

    Upgrade gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,38 @@
+var makeBinary = true;
+
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
     gp_concat = require('gulp-concat'),
     gp_rename = require('gulp-rename'),
     gp_uglify = require('gulp-uglify');
 
-var dg_toggleModuleSrc = [
+var jsSrc = [
   './src/_dg_toggle.js',
   './src/classes/class.*.js',
-  './src/blocks/blocks.*.js',
-  './src/forms/form.*.js',
   './src/includes/include.*.js',
-  './src/pages/page.*.js',
   './src/widgets/widget.*.js'
 ];
 
-// Task to build the cw_app.min.js file.
-gulp.task('minifyJS', function(){
-  return gulp.src(dg_toggleModuleSrc)
-      .pipe(gp_concat('concat.js'))
-      .pipe(gulp.dest(''))
-      .pipe(gp_rename('dg_toggle.min.js'))
-      .pipe(gp_uglify())
-      .pipe(gulp.dest(''));
-});
+// Minify JavaScript
+function minifyJs() {
+  console.log('compressing dg_toggle.js...');
+  var js = gulp.src(jsSrc)
+    .pipe(gp_concat('dg_toggle.js'))
+    .pipe(gulp.dest('./'));
+  if (makeBinary) {
+    console.log('compressing dg_toggle.min.js...');
+    return js.pipe(gp_rename('dg_toggle.min.js'))
+    .pipe(gp_uglify())
+    .pipe(gulp.dest('./'));
+  }
+  return js;
+}
+gulp.task(minifyJs);
 
-gulp.task('default', function () {
-  watch(dg_toggleModuleSrc, function(event) { gulp.start('minifyJS') });
-  gulp.start('minifyJS');
+gulp.task('default', function(done) {
+
+  gulp.watch(jsSrc, gulp.series('minifyJs'));
+
+  done();
+
 });
